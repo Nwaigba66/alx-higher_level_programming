6e588e37f4caecff3efd17bc06f9d7022e31fbdb
Nwaigba66 <glorianwaigba@gmail.com>
2023-05-08 16:05:52 -0700

    Python programming

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-number = 3.14159
 
 # Print the float stored in the variable number with a precision of 2 digits
-print(f"Float: {number:.2f}\n")
+number = 3.14159
+print(f"Float: {number:.2f}")
 
