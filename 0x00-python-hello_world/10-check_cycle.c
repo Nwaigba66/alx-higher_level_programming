f64c1c353e65ac8a68fca616f6dd96274706b746
Nwaigba66 <glorianwaigba@gmail.com>
2023-05-08 16:55:35 -0700

    Python programming

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,30 @@
-#include <stddef.h>
+#include "lists.h"
+#include <stdio.h>
 
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
+ * check_cycle - checks if singly linked list is a cycle
+ * Return: 0 if no cycle, 1 is yes
  */
-
-int check_cycle(struct ListNode *list)
+int check_cycle(listint_t *list)
 {
-    struct ListNode *slow = list;
-    struct ListNode *fast = list;
-    
-    while (fast != NULL && fast->next != NULL)
-	{
-        slow = slow->next;
-        fast = fast->next->next;
-        
-        if (slow == fast) {
-            return 1;
-        }
-    }
-    
-    return 0;
-}
+	listint_t *fast = list;
+	listint_t *slow = list;
 
+	if (!list)
+		return (0);
+
+	while (1)
+	{
+		if (fast->next != NULL && fast->next->next != NULL)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+
+			if (fast == slow)
+				return (1);
+		}
+		else
+			return (0);
+	}
+
+}
