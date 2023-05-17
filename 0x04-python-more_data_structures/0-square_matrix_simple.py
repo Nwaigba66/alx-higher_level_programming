6b8ff629ae3d7daa31ba08c430a990b4fdb1bed3
Nwaigba66 <glorianwaigba@gmail.com>
2023-05-17 14:43:15 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix of the same size as the input matrix
-    result_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-    
-    # Iterate over each element in the matrix and compute its square
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            result_matrix[i][j] = matrix[i][j] ** 2
-    
-    return result_matrix
-
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
