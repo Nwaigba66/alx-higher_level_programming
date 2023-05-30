973a789425a70a8d1e68cc76938f3a77b8aa0704
Nwaigba66 <glorianwaigba@gmail.com>
2023-05-29 18:50:08 -0700

    Second commit Python - Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {}".format(result))
-        return result
     except ZeroDivisionError:
-        print("Inside result: None")
         return None
+    finally:
+        print("Inside result: {}".format(result))
