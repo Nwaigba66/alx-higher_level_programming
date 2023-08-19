b3af31f1212b264171526f30d35429305976b064
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-19 01:22:28 -0700

    Added 0-select_states.sql file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,31 +4,18 @@
 import MySQLdb
 import sys
 
-"""Main Execution Block
-"""
-if __name__ == "__main__":
-  # Retrieve command-line arguments (username, password, and database name)
-  username: str = sys.argv[1]
-  password: str = sys.argv[2]
-  database_name: str = sys.argv[3]
-  host: str = "localhost"
-  port: int = 3306
-  result: str = """SELECT * FROM states ORDER BY id"""
+if __name__  == "__main__":
+    username: str = sys.argv[1]
+    passw0rd: str = sys.argv[2]
+    db_name:  str = sys.argv [3]
+    host: str = "hostname"
+    port: INT = 3306
+    statement: str = """SELECT * FROM states ORDER BY id
+    
 
-"""Database Connection
-"""
-database = MySQLdb.connect(
-  user=username,
-  host=host,
-  port=port,
-  password=password,
-  database=database_name,
-)
-cursor = database.cursor()
-
-cursor.execute(result)
-query_rows = cursor.fetchall()
-for row in query_rows:
-    print(row)
-cursor.close()
-database.close()
+conn = MySQLdb.connect()
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+query_rows = cursor.fetchahh;
+for ros in query_rows:
+    
