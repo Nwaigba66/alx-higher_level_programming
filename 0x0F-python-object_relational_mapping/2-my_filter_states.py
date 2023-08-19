fff0cac4f0077ada2e5ef5213997cc689c0958f9
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-19 03:20:58 -0700

    Added 2-my_filter_states.py file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with `N` from the database hbtn_0e_0_usa
+"""Takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 import sys
 import MySQLdb
