867cfe2354e0e9a9296f856abafaf0420ae7aef3
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-31 18:35:44 -0700

    Updated code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a function that finds a peak in unsorted list"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """ Find peak in an unsorted list """
+    """Finds the peak in a list of integers."""
+    length = len(list_of_integers)
 
-    # return None if not list or if empty
-    if not list_of_integers:
-        return
-    left_index = 0
-    right_index = len(list_of_integers) - 1
+    if length == 0:
+        return None
 
-    # keep the search going until left index == right index
-    while left_index != right_index:
-        middle_index = int((left_index+right_index)/2)
+    down, up = 0, length - 1
 
-        if list_of_integers[middle_index] < list_of_integers[middle_index + 1]:
-            left_index = middle_index + 1
-        else:  
-            right_index = middle_index
-    # return the number (peak) at the convergent left and right index
-    return list_of_integers[right_index]
+    while down < up:
+        mid = (down + up) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            down = mid + 1
+        else:
+            up = mid
+
+    return list_of_integers[down]
