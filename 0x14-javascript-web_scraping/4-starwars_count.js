48a744269d5bedb62a9678eee1a13759d61a2aa6
Gloria Nwaigba <glorianwaigba@gmail.com>
2023-09-29 05:14:54 -0700

    Updated the script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
-//const request = require('request');
-//const url = process.argv[2];
-//const character_id = '18';
+// const request = require('request');
+// const url = process.argv[2];
+// const character_id = '18';
 
-//request(url, function(error, response, body) {
- // if (!error){
-    //const results = JSON.parse(body).results;
-    //const filteredResult = results.filter((film) => {
-      //const {characters} = film;
-      //characters.forEach((character) => {
-      //  if (character.endsWith('/18/')) return true;
-    //  }) 
-  //    return false;
+// request(url, function(error, response, body) {
+// if (!error){
+// const results = JSON.parse(body).results;
+// const filteredResult = results.filter((film) => {
+// const {characters} = film;
+// characters.forEach((character) => {
+//  if (character.endsWith('/18/')) return true;
+//  })
+//    return false;
 //    });
 // console.log(filteredResult.length);
- // }
+// }
 // });
- 
+
 const request = require('request');
 const url = process.argv[2];
-const character_id = '18';
+const characterId = '18';
 
-request(url, function(error, response, body) {
+request(url, function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     const filteredResult = results.filter((film) => {
       const { characters } = film;
       let characterFound = false; // Initialize a flag
       characters.forEach((character) => {
-        if (character.endsWith('/' + character_id + '/')) {
+        if (character.endsWith('/' + characterId + '/')) {
           characterFound = true; // Set the flag to true if the condition is met
         }
       });
@@ -37,4 +37,3 @@ request(url, function(error, response, body) {
     console.log(filteredResult.length);
   }
 });
-
