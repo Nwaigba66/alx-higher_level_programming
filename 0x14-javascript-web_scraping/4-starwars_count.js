0419753fe2c517ff6f85c6688215f1b14d989b8b
Gloria Nwaigba <glorianwaigba@gmail.com>
2023-09-29 05:11:05 -0700

    Updated the script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,40 @@
 #!/usr/bin/node
+//const request = require('request');
+//const url = process.argv[2];
+//const character_id = '18';
+
+//request(url, function(error, response, body) {
+ // if (!error){
+    //const results = JSON.parse(body).results;
+    //const filteredResult = results.filter((film) => {
+      //const {characters} = film;
+      //characters.forEach((character) => {
+      //  if (character.endsWith('/18/')) return true;
+    //  }) 
+  //    return false;
+//    });
+// console.log(filteredResult.length);
+ // }
+// });
+ 
 const request = require('request');
 const url = process.argv[2];
 const character_id = '18';
 
 request(url, function(error, response, body) {
-  if (!error){
+  if (!error) {
     const results = JSON.parse(body).results;
     const filteredResult = results.filter((film) => {
-      const {characters} = film;
+      const { characters } = film;
+      let characterFound = false; // Initialize a flag
       characters.forEach((character) => {
-        if (character.endsWith('/18/')) return true;
-      }) 
-      return false;
+        if (character.endsWith('/' + character_id + '/')) {
+          characterFound = true; // Set the flag to true if the condition is met
+        }
+      });
+      return characterFound; // Return the flag's value
     });
     console.log(filteredResult.length);
   }
 });
-  
+
